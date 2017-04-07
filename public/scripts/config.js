3e8b210cebf02e33313caf5c3856299e6fa3c232
Chad Sundberg <chadsundberg@gmail.com>
2017-04-07 11:35:13 -0500

    first commit

---
+++ public/scripts/config.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDCLuD660GZ3btSypZDN_7dTKENHN5HsB0",
+    authDomain: "bet-shalom-volunteer-first.firebaseapp.com",
+    databaseURL: "https://bet-shalom-volunteer-first.firebaseio.com",
+    projectId: "bet-shalom-volunteer-first",
+    storageBucket: "bet-shalom-volunteer-first.appspot.com",
+    messagingSenderId: "933731433357"
+  };
+  firebase.initializeApp(config);
