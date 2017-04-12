ec1287fb76fd7243f129e4b209b24bea697216f4
Jonny Holupchinski <jonholup@gmail.com>
2017-04-12 11:39:13 -0500

    simple navbar added w/ some styling

--- public/scripts/controllers/login.controller.js
+++ public/scripts/controllers/login.controller.js
@@ -39,42 +39,16 @@ app.controller("LoginController", ["DataFactory", "$location", "$firebaseAuth", 
       });
   };
 
-
-  // This code runs whenever the user changes authentication states
-  // e.g. whevenever the user logs in or logs out
-  // this is where we put most of our logic so that we don't duplicate
-  // the same things in the login and the logout code
-  // auth.$onAuthStateChanged(function(firebaseUser){
-  //   // firebaseUser will be null if not logged in
-  //   if(firebaseUser) {
-  //     // This is where we make our call to our server
-  //     firebaseUser.getToken().then(function(idToken){
-  //       $http({
-  //         method: 'GET',
-  //         url: '/privateData',
-  //         headers: {
-  //           id_token: idToken
-  //         }
-  //       }).then(function(response){
-  //         self.secretData = response.data;
-  //         console.log('secretData:', secretData);
-
-  //       });
-  //     });
-  //   } else {
-  //     console.log('Not logged in or not authorized.');
-  //     self.secretData = [];
-  //   }
-
-
-  // });
-
-
   // This code runs when the user logs out
-  self.logOut = function () {
+  self.signOut = function () {
     auth.$signOut().then(function () {
       console.log('Logging the user out!');
       $location.path('/');
     });
   };
+
+  auth.$onAuthStateChanged(function (firebaseUser) {
+    self.userIsLoggedIn = firebaseUser !== null;
+    self.userIsLoggedOut = firebaseUser === null;
+  });
 }]);
