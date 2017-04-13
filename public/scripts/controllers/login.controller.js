6cd7273e89e57d89bedee4a95dda422c53e68913
Chad Sundberg <chadsundberg@gmail.com>
2017-04-13 10:10:12 -0500

    log-in controller

--- public/scripts/controllers/login.controller.js
+++ public/scripts/controllers/login.controller.js
@@ -21,31 +21,6 @@ app.controller("LoginController", ["DataFactory", "$location", "$firebaseAuth", 
           DataFactory.addUser(self.newUser);
           self.newUser = {};
 
-  // This code runs whenever the user changes authentication states
-  // e.g. whevenever the user logs in or logs out
-  // this is where we put most of our logic so that we don't duplicate
-  // the same things in the login and the logout code
-  auth.$onAuthStateChanged(function(firebaseUser){
-    // firebaseUser will be null if not logged in
-    if(firebaseUser) {
-      // This is where we make our call to our server
-      firebaseUser.getToken().then(function(idToken){
-        $http({
-          method: 'GET',
-          url: '/privateData',
-          headers: {
-            id_token: idToken
-          }
-        }).then(function(response){
-          self.secretData = response.data;
-          console.log('secretData:', secretData);
-
-        });
-      });
-    } else {
-      console.log('Not logged in or not authorized.');
-      self.secretData = [];
-    }
           self.message = "User created with uid: " + firebaseUser.uid;
           console.log("Firebase Authenticated as: ", firebaseUser.newUser.email);
           $location.path('/home');
