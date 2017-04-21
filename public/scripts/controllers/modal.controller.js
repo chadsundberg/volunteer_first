3bff3e68184317a098f2066ed4e6bb0dd87ab257
Jonny Holupchinski <jonholup@gmail.com>
2017-04-21 17:30:12 -0500

    duration from > 0 --> 0 resets

--- public/scripts/controllers/modal.controller.js
+++ public/scripts/controllers/modal.controller.js
@@ -1,4 +1,4 @@
-app.controller("ModalInstanceCtrl", ["DataFactory", "ModalDataFactory", "$location", "$firebaseAuth", "$http", "$uibModalInstance", "title",  function(DataFactory, ModalDataFactory, $location, $firebaseAuth, $http, $uibModalInstance, title) {
+app.controller("ModalInstanceCtrl", ["DataFactory", "ModalDataFactory", "$location", "$firebaseAuth", "$http", "$uibModalInstance", "title", function (DataFactory, ModalDataFactory, $location, $firebaseAuth, $http, $uibModalInstance, title) {
   console.log('Modal Controller was loaded');
   var $ctrl = this;
 
@@ -22,16 +22,20 @@ app.controller("ModalInstanceCtrl", ["DataFactory", "ModalDataFactory", "$locati
   $ctrl.adminAddRole = DataFactory.adminAddRole;
   $ctrl.deleteRole = DataFactory.deleteRole;
   $ctrl.editRole = DataFactory.editRole;
+  $ctrl.getUsers = DataFactory.getUsers;
+  $ctrl.getEvents = DataFactory.getEvents;
+  $ctrl.getCurrentDuration = DataFactory.getCurrentDuration;
+  $ctrl.getUserData = DataFactory.getUserData;
 
   if ($ctrl.eventId === undefined) {
-      $ctrl.eventRoles.list = {};
+    $ctrl.eventRoles.list = {};
   } else {
     $ctrl.getEventRoles($ctrl.eventId);
   }
 
-if($ctrl.eventId === undefined){
-  $ctrl.eventRoles.list = {};
-} else {
+  if ($ctrl.eventId === undefined) {
+    $ctrl.eventRoles.list = {};
+  } else {
     $ctrl.getEventRoles($ctrl.eventId);
   }
 
@@ -45,63 +49,72 @@ if($ctrl.eventId === undefined){
   };
 
 
-//USER SIGN UP FUNCTIONS
-//Role on click to database
+  //USER SIGN UP FUNCTIONS
+  //Role on click to database
 
-$ctrl.clickSaveSignUp = function(roleClickedId){
-  $ctrl.volunteerSignUp($ctrl.eventId, roleClickedId);
-  console.log(roleClickedId);
-};
+  $ctrl.clickSaveSignUp = function (roleClickedId) {
+    $ctrl.volunteerSignUp($ctrl.eventId, roleClickedId);
+    console.log(roleClickedId);
+  };
 
+  $ctrl.exitModal = function (roleClickedId) {
+    $ctrl.getUsers();
+    $ctrl.getEvents();
 
-// Melissa
-$ctrl.clickRemove = function(roleClickedId){
-  $ctrl.volunteerRemove($ctrl.eventId, roleClickedId);
-  console.log(roleClickedId);
-};
-
-// MELISSA STUFF!!
-    $ctrl.selected="";
-      $ctrl.users = DataFactory.users.list;
-      // ['Alabama', 'Alaska', 'Arizona', 'Arkansas',
-      // 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida',
-      // 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
-      // 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
-      // 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
-      // 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-      // 'New Jersey', 'New Mexico', 'New York', 'North Dakota',
-      // 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
-      // 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
-      // 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
-      // 'West Virginia', 'Wisconsin', 'Wyoming'];
-
-console.log($ctrl.users);
+    // $ctrl.getUserData();
+    $ctrl.getCurrentDuration();
+    $uibModalInstance.dismiss('cancel');
+  };
 
 
+  // Melissa
+  $ctrl.clickRemove = function (roleClickedId) {
+    $ctrl.volunteerRemove($ctrl.eventId, roleClickedId);
+    console.log(roleClickedId);
+  };
+
+  // MELISSA STUFF!!
+  $ctrl.selected = "";
+  $ctrl.users = DataFactory.users.list;
+  // ['Alabama', 'Alaska', 'Arizona', 'Arkansas',
+  // 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida',
+  // 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
+  // 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
+  // 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
+  // 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+  // 'New Jersey', 'New Mexico', 'New York', 'North Dakota',
+  // 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
+  // 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
+  // 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
+  // 'West Virginia', 'Wisconsin', 'Wyoming'];
+
+  console.log($ctrl.users);
 
 
-// Please note that the close and dismiss bindings are from $uibModalInstance.
-angular.module('volunteerApp').component('modalComponent', {
-  templateUrl: 'myModalContent.html',
-  bindings: {
-    resolve: '<',
-    close: '&',
-    dismiss: '&'
-  },
-  controller: function () {
-    var $ctrl = this;
-    $ctrl.$onInit = function () {
-      $ctrl.items = $ctrl.resolve.items;
-      $ctrl.selected = {
-        item: $ctrl.items[0]
+
+
+  // Please note that the close and dismiss bindings are from $uibModalInstance.
+  angular.module('volunteerApp').component('modalComponent', {
+    templateUrl: 'myModalContent.html',
+    bindings: {
+      resolve: '<',
+      close: '&',
+      dismiss: '&'
+    },
+    controller: function () {
+      var $ctrl = this;
+      $ctrl.$onInit = function () {
+        $ctrl.items = $ctrl.resolve.items;
+        $ctrl.selected = {
+          item: $ctrl.items[0]
+        };
       };
-    };
-    $ctrl.ok = function () {
-      $ctrl.close({$value: $ctrl.selected.item});
-    };
-    $ctrl.cancel = function () {
-      $ctrl.dismiss({$value: 'cancel'});
-    };
-  }
-});
+      $ctrl.ok = function () {
+        $ctrl.close({ $value: $ctrl.selected.item });
+      };
+      $ctrl.cancel = function () {
+        $ctrl.dismiss({ $value: 'cancel' });
+      };
+    }
+  });
 }]);
