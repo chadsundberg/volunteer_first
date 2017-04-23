a7e28087b5c2e51e7c9c1a0b5385a5fc5541033d
Christine Pogatchnik <christinepogatchnik@gmail.com>
2017-04-23 16:46:00 -0500

    okay

--- public/scripts/controllers/calendar.controller.js
+++ public/scripts/controllers/calendar.controller.js
@@ -19,7 +19,7 @@ app.controller("CalendarController", ["DataFactory", "ModalDataFactory", "$locat
   self.currentUser = DataFactory.currentUser;
   self.getEventRoles = DataFactory.getEventRoles;
   self.getCurrentDuration = DataFactory.getCurrentDuration;
-  self.getCurrentDuration();
+
 
   // state change / refresh
   //  auth.$onAuthStateChanged(function (firebaseUser) {
